b0c129b19b4cfb314146fc2cc0960472cd736697
AntoineCor35 <antoine.cormier@my-digital-school.org>
2025-05-13 11:23:23 +0200

    Ajout de Jest pour les tests unitaires

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@testing-library|axios)/)'
+  ],
+}; 
