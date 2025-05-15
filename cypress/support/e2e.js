b671dd59616f8005c57fc71f1ee1be31b6f1bf9f
AntoineCor35 <antoine.cormier@my-digital-school.org>
2025-05-15 15:03:36 +0200

    test(e2e): ajustements ou hooks dans le support Cypress

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,1 +1,15 @@
 // Support file for Cypress E2E tests
+
+Cypress.Commands.add('loginByApi', (pseudo, password) => {
+  cy.request('POST', 'http://localhost:3001/auth/login', { pseudo, password })
+    .then((response) => {
+      // Log complet de la réponse pour debug
+      // eslint-disable-next-line no-console
+      console.log('Réponse API login:', response);
+      expect(response.status).to.eq(200);
+      const { access_token } = response.body;
+      window.localStorage.setItem('token', access_token);
+      // Décoder le token pour stocker l'utilisateur (optionnel, selon l'app)
+      // window.localStorage.setItem('user', JSON.stringify(jwtDecode(access_token)));
+    });
+});
